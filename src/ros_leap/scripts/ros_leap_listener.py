69f4bfff52a87bd3b6839684db2ac5ac3b359b95
YANG Qiao <yangqiao0505@me.com>
2017-04-05 00:42:46 +0200

    init commit ros_leap

---
+++ src/ros_leap/scripts/ros_leap_listener.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+import signal
+import rospy
+import Leap
+from ros_leap.msg import Leap_Frame, Hand, Finger, Bone, Arm, Vector3
+
+
+def listener():
+	finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
+	bone_names = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
+
+	def callback(leap_msg):
+
+		rospy.loginfo(leap_msg)
+		rospy.loginfo("A Leap Frame received")
+		# end of callback
+
+	rospy.init_node(node_name, anonymous=True)
+	rospy.Subscriber(topic_name, Leap_Frame, callback)
+	rospy.spin()
+
+if __name__ == "__main__":
+	""" LeapMotion data listener
+
+	This script will create an anonymous ros node,
+	listen over topic "ros_ar10_set_servo_positions"
+
+	Can be used for a listener sample code for LM data 
+
+	"""
+	# store the original SIGINT handler
+	topic_name = 'ros_leap_frame'
+	node_name = 'ros_leap_listener'
+	listener()
